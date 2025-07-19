0a4f4f3e6cb3a4d879d9c903f04b31695ec15e9c
Babs Oyewumi <babalakin7@gmail.com>
2025-07-19 19:23:22 -0400

    Created first blog

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -5,6 +5,13 @@ interface Project {
   imgSrc?: string
 }
 
-const projectsData: Project[] = []
+const projectsData: Project[] = [
+  {
+    title: 'Babs UI Library',
+    description: `A modern, developer-first UI component library built with React and TypeScript. Designed for speed, consistency, and scalability—perfect for design systems and product interfaces.`,
+    imgSrc: '/static/images/babs-ui.png',
+    href: 'https://github.com/boyewumi/babs-ui',
+  },
+]
 
 export default projectsData
